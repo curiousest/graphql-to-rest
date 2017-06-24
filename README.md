21a4d39753401e843d75f070bfe9a9b30b0a2941
Douglas <dmhindson@gmail.com>
2017-06-24 18:52:27 +0100

    Initial.

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# graphql-to-rest
-Makes any REST API compatible with GraphQL
+# graphql-to-REST
+
+Makes any REST API compatible with [GraphQL](http://graphql.org/learn/) requests.
+
+It can:
+
+* Sit as a standalone Flask app in front/behind your authentication layer, somewhere in between your frontend and services' APIs
+* Communicate to services/APIs spanning multiple hosts
+* Conform to different [opinions](https://cloud.google.com/apis/design/) [of](https://hackernoon.com/restful-api-designing-guidelines-the-best-practices-60e1d954e7c9) [how](https://docs.atlassian.com/jira/REST/cloud/) [REST](https://github.com/Microsoft/api-guidelines/blob/vNext/Guidelines.md) [should](https://docs.stormpath.com/rest/product-guide/latest/reference.html) [work](http://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api)
+
+It can't:
+
+* Modify data
+
+You must:
+
+* Define your API schema in [graphene](https://github.com/graphql-python/graphene) (example below)
+* Run a flask app [like this](https://github.com/curiousest/graphql-to-rest-example) somewhere
+
+## Example Usage
+
+[Here is a repository you can fork and use.](https://github.com/curiousest/graphql-to-rest-example)
+
+### Schema definition
+
+```python
+import graphene
+from external_rest import ExternalRESTObject
+
+class Hero(ExternalRESTObject):
+    endpoint = "http://your-host/heroes"
+    
+    id = graphene.Int()
+    name = graphene.String(name='name')
+    friend_ids = graphene.List(graphene.Int)
+    friends = graphene.List(
+        partial(lambda: Hero),
+        resolver=partial(lambda *args, **kwargs: Hero.generate_resolver(
+            filter_to_source_dict={'id': 'friend_ids'}, is_list=True
+        )(*args, **kwargs))
+    )
+
+class Query(graphene.ObjectType):
+
+    heroes = graphene.List(
+        Hero,
+        id=graphene.Argument(graphene.ID),
+        resolver=Hero.generate_resolver(filter_by_parent_fields=False)
+    )
+
+schema = graphene.Schema(query=Query)
+```
+
+### GraphQL Request / Response
+
+```python
+import requests
+import json
+
+query = '''
+{
+    heroes (id: "5") {
+        id
+        friends {
+            id
+            name
+        }
+    }
+}
+'''
+data = {'query': query}
+response = requests.get(
+    "http://your-host/graphql/", 
+    data=json.dumps(data), 
+    content_type='application/json'
+)
+print(response.json())
+
+> {"data":{"heroes":[{"id":5,"friends":[{"id":6,"name":"Obi"},{"id":7,"name":"Yoda"}]}]}}
+
+```
+
+## Testing
+
+```
+py.test
+py.test --capture=no # if you want to `import pytest; pytest.set_trace()`
+```
---
+++ tests/test_asdf.py
@@ -0,0 +1,247 @@
+import pytest
+import requests_mock
+import json
+
+
+'''
+Cases come from http://graphql.org/learn/queries/
+
+Imagine the endpoints:
+
+Heroes
+url: /heroes/
+filters: id, faction_id
+fields: id, name, height, friend_ids, faction_id
+
+ex: GET /heroes/?id=5
+[{
+    'id': 5,
+    'name': 'Luke',
+    'height': 1.25,
+    'friend_ids': [6, 7],
+    'faction_id': 1
+}]
+
+Factions
+url: /factions/
+filters: id
+fields: id, name
+
+ex: GET /factions/?id=1
+[{
+    'id': 1,
+    'name': 'jedi',
+}]
+
+'''
+
+hero_1_data = {
+    'id': 5,
+    'name': 'Luke',
+    'height': 1.25,
+    'friend_ids': [6, 7],
+    'faction_id': 1,
+}
+hero_2_data = {
+    'id': 6,
+    'name': 'Obi',
+    'height': 1.3,
+    'friend_ids': [5, 7],
+    'faction_id': 1,
+}
+hero_3_data = {
+    'id': 7,
+    'name': 'Yoda',
+    'height': 0.5,
+    'friend_ids': [5, 6],
+    'faction_id': 1,
+}
+
+faction_1_data = {
+    'id': 1,
+    'name': 'jedi'
+}
+
+
+class ExternalRESTTestClass:
+
+    def setUp(self):
+        assert self.graphql_host is not None, \
+            "Subclass this and add a host"
+
+    def test_single_related_field_by_id(self, client):
+        query = '''
+        {
+            heroes (id: "5") {
+                id
+                faction {
+                    id,
+                    name
+                }
+            }
+        }
+        '''
+        data = {'query': query}
+
+        with requests_mock.mock() as m:
+            m.get(
+                'http://test/heroes/?id=5',
+                json={
+                    'results': [hero_1_data]
+                })
+            m.get('http://test/factions/1/', json=faction_1_data)
+            response = client.post(
+                self.graphql_host,
+                data=json.dumps(data),
+                content_type='application/json',
+            )
+        assert response.status_code == 200, "{} error: {}".format(
+            response.status_code, response.data
+        )
+        json_response = json.loads(response.data.decode())
+        assert 'errors' not in json_response
+        assert json_response['data']['heroes'][0]['id'] == 5
+        assert json_response['data']['heroes'][0]['faction']['id'] == 1
+
+    def test_multiple_related_field_by_parent_id(self, client):
+        query = '''
+        {
+            factions (id: "1") {
+                id
+                name
+                heroes {
+                    id
+                    name
+                }
+            }
+        }
+        '''
+        data = {'query': query}
+
+        with requests_mock.mock() as m:
+            m.get(
+                'http://test/factions/?id=1',
+                json={
+                    'results': [faction_1_data]
+                })
+            m.get('http://test/heroes/?faction_id=1', json={
+                'results': [hero_1_data, hero_2_data, hero_3_data]
+            })
+
+            response = client.post(
+                self.graphql_host,
+                data=json.dumps(data),
+                content_type='application/json',
+            )
+
+        assert response.status_code == 200, "{} error: {}".format(
+            response.status_code, response.data
+        )
+        json_response = json.loads(response.data.decode())
+
+        assert 'errors' not in json_response
+        assert json_response['data']['factions'][0]['id'] == 1
+        assert 5 in [a['id']
+                     for a in json_response['data']['factions'][0]['heroes']]
+
+    def test_multiple_related_field_by_ids(self, client):
+        query = '''
+        {
+            heroes (id: "5") {
+                id
+                friends {
+                    id
+                    name
+                }
+            }
+        }
+        '''
+        data = {'query': query}
+
+        with requests_mock.mock() as m:
+            m.get(
+                'http://test/heroes/?id=5',
+                json={
+                    'results': [hero_1_data]
+                })
+            m.get('http://test/heroes/?id=6,7', json={
+                'results': [hero_2_data, hero_3_data]
+            })
+
+            response = client.post(
+                self.graphql_host,
+                data=json.dumps(data),
+                content_type='application/json',
+            )
+        assert response.status_code == 200, "{} error: {}".format(
+            response.status_code, response.data
+        )
+        json_response = json.loads(response.data.decode())
+
+        assert 'errors' not in json_response, "error: {}".format(
+            response.data
+        )
+        assert json_response['data']['heroes'][0]['id'] == 5
+        assert json_response['data']['heroes'][0]['friends'][0]['id'] == 6
+        assert json_response['data']['heroes'][0]['friends'][1]['id'] == 7
+
+    def test_passes_query_params(self, client):
+        query = '''
+        {
+            factions (id: "1") {
+                id
+            }
+        }
+        '''
+        data = {'query': query}
+
+        with requests_mock.mock() as m:
+            m.get(
+                'http://test/factions/?this_query_param=exists&that_query_param=exists&id=1',
+                json={
+                    'results': [faction_1_data]
+                })
+            response = client.post(
+                self.graphql_host,
+                query_string={
+                    'this_query_param': 'exists', 'that_query_param': 'exists'
+                },
+                data=json.dumps(data),
+                content_type='application/json',
+            )
+        # this will error if the mock get url (with query params) doesn't
+        # match what is requested
+        pass
+
+    def test_passes_headers(self, client):
+        query = '''
+        {
+            factions (id: "1") {
+                id
+            }
+        }
+        '''
+        data = {'query': query}
+
+        with requests_mock.mock() as m:
+            m.get(
+                'http://test/factions/?id=1',
+                json={
+                    'results': [faction_1_data]
+                })
+            response = client.post(
+                self.graphql_host,
+                data=json.dumps(data),
+                content_type='application/json',
+                headers={'X-THIS-HEADER': 'exists'}
+            )
+        assert m.request_history[0]._request.headers['X-This-Header'] == 'exists'
+
+
+class TestCompressedSchema(ExternalRESTTestClass):
+    graphql_host = '/graphql/compressed'
+
+
+class TestExpressiveSchema(ExternalRESTTestClass):
+    graphql_host = '/graphql/expressive'
+
