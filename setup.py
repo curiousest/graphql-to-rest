afc300a121d5e6bbc0af8ef7bb9e6f8d227aff2a
Douglas <dmhindson@gmail.com>
2017-09-27 14:36:25 +0700

    Add author details to setup.py.

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version='1.0',
     description='Make any REST API compatible with GraphQL',
     url='https://github.com/curiousest/graphql-to-rest',
+    author='Douglas Hindson',
+    author_email='dmhindson+pypi@gmail.com',
     packages=find_packages(),
     install_requires=[
         'requests',
